8b638a62d75d8c8ed777f0c5e25eb1189cfb63ff
rei315 <alsrnr315@gmail.com>
2023-05-01 16:11:18 +0900

    add file

---
+++ Retty/RettyTest.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,25 @@
+//___FILEHEADER___
+
+import Nimble
+import Quick
+
+@testable import Retty
+
+final class ___FILEBASENAMEASIDENTIFIER___: QuickSpec {
+    struct TestCase {
+        let context: String
+        let <#propertyName#>: <#PropertyType#>
+    }
+
+    let validTestCases: [TestCase] = [
+        
+    ]
+    
+    let invalidTestCases: [TestCase] = [
+        
+    ]
+    
+    override func spec() {
+        
+    }
+}
